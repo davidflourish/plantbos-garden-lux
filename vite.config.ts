627c367a10b477d49f379fde18fec00ed450bc56
davidflourish <126780643+davidflourish@users.noreply.github.com>
2026-05-19 13:11:29 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite' // or solid equivalent
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { nitro } from 'nitro/vite'
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     tanstackStart(),
     nitro({
-      preset: 'vercel'   // ← This is key
+      preset: 'vercel'
     }),
     viteReact(),
   ],
