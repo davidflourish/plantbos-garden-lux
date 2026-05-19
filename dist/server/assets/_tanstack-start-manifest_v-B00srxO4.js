39b16aa499e3617bad73d27054969ccc74094c62
davidflourish <126780643+davidflourish@users.noreply.github.com>
2026-05-19 20:46:27 +0100

    Add files via upload

---
+++ dist/server/assets/_tanstack-start-manifest_v-B00srxO4.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Windows/Desktop/plantbos-garden-lux-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-BgLWq9lc.js"] }, "/": { filePath: "C:/Users/Windows/Desktop/plantbos-garden-lux-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CzGLjHuI.js"] } }, clientEntry: "/assets/index-BgLWq9lc.js" });
+export {
+  tsrStartManifest
+};
